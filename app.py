adc666aac584adffb58caf2b2f4f511c46ea32c6
anadeduck <ana.clara.deduck@gmail.com>
2024-10-22 00:22:22 -0300

    #metodo POST

--- app.py
+++ app.py
@@ -1,28 +1,22 @@
 # URL: https://medium.com/@adilmarcoelhodantas/introdu%C3%A7%C3%A3o-%C3%A0s-%C3%A1rvores-de-decis%C3%A3o-aplica%C3%A7%C3%B5es-e-exemplo-pr%C3%A1tico-com-python-13dccdfb1030
-
-
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-   print('Log: Show the home.html')
-   return render_template('home.html')
+    print('Log: Show the home.html')
+    return render_template('home.html')
 
-@app.route("/knn")
-def knn():
-   
-    # Get the variables from query parameters
-    # Ex. url/knn?var1=10&var2=100&var3=1000
-    print('Log: Receive var from GET')
-    print('Log: Value of K = ', request.args.get('k'))
+@app.route("/decisionTree", methods=["POST"])
+def decisionTree():
+    # Captura os parâmetros enviados pelo formulário (método POST)
+    print('Log: Receive var from POST')
+    print('Log: Value of K = ', request.form.get("k"))
 
-    # Here put the knn code basead on the vars received
-    #
+    # Aqui você pode inserir o código da árvore de decisão baseado nas variáveis recebidas
+    # Exemplo de processamento
 
-
-
-    # Bellow return the resul of the knn code
+    # Abaixo retorna o resultado da execução da árvore de decisão
     return 'Deu certo'
 
 if __name__ == "__main__":
