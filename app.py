30a035ce4de35c9d158ba1e89e4d67529c9e41c0
anadeduck <ana.clara.deduck@gmail.com>
2024-10-29 20:02:10 -0300

    refactoring, added function answerDecisionTree

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 # URL: https://medium.com/@adilmarcoelhodantas/introdu%C3%A7%C3%A3o-%C3%A0s-%C3%A1rvores-de-decis%C3%A3o-aplica%C3%A7%C3%B5es-e-exemplo-pr%C3%A1tico-com-python-13dccdfb1030
-from flask import Flask, request, render_template
+import DecisionTree
+from flask import Flask, request, render_template, jsonify
+
+from DecisionTree import answerDecisionTree
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,17 +11,19 @@ def home():
     print('Log: Show the home.html')
     return render_template('home.html')
 
-@app.route("/decisionTree", methods=["POST"])
-def decisionTree():
+@app.route("/Jogartenis", methods=["POST"])
+def Jogartenis():
     # Captura os parâmetros enviados pelo formulário (método POST)
     print('Log: Receive var from POST')
     print('Log: Value of K = ', request.form.get("k"))
+    result = answerDecisionTree()
+
 
     # Aqui você pode inserir o código da árvore de decisão baseado nas variáveis recebidas
     # Exemplo de processamento
 
     # Abaixo retorna o resultado da execução da árvore de decisão
-    return 'Deu certo'
+    return jsonify({"result": result})
 
 if __name__ == "__main__":
     app.run()
